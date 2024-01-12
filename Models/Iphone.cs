b603997d759eb44e71d912e252ef9ac89a9e5987
thiagoether <thiagomarques92@gmail.com>
2024-01-12 17:56:18 -0300

    Desafio concluido!

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,20 @@
+using System.ComponentModel;
+using System.Reflection.Metadata;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instando aplicativo {nomeApp} no Iphone.");
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("NOKIA");
+Smartphone nokia = new Nokia(numero:"123456789", modelo: "n95", imei: "5a4c2e3b", memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("YouTube");
+
+Console.WriteLine("=========================");
+
+Console.WriteLine("APPLE IPHONE");
+Smartphone iphone = new Iphone(numero:"987654321", modelo:"7 plus", imei:"9f8c5a4b", memoria:256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("=========================");
+
+Console.WriteLine("IPHONE MAIS NOVO");
+Smartphone iphone2 = new Iphone(numero:"997654321", modelo:"X", imei:"1q2w3e4r5t", memoria:128);
+iphone2.Ligar();
+iphone2.InstalarAplicativo("Telegram");
